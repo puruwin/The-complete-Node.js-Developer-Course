9b67ccd395c65e22468805f2bc3ca577b1582684
David Pérez Martínez <taborlindp@gmail.com>
2022-05-16 12:19:40 +0200

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,30 @@ const request = require('request')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'David Perez'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'David Perez'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'We are sending help!',
+        name: 'David Perez'
+    })
+})
 
 app.get('/weather', (req, res) => {
     const url = 'http://api.weatherstack.com/current?access_key=4fa109becd811329cf52f7bd471d3a7c&query=38.5412,0.1234'
