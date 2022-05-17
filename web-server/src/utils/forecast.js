f21cb565e76aeb1b913456d44c8229818fc96539
David Pérez Martínez <taborlindp@gmail.com>
2022-05-17 17:25:19 +0200

    Browser HTTP Request with Fetch

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             console.log(body.error)
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.location.name + ', ' + body.location.country + ': ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like ' + body.current.feelslike + '.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like ' + body.current.feelslike + '.')
         }
     })
 }
