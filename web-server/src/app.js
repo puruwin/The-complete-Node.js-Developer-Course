449956511852359a40ab23a31caad654321d383c
David Pérez Martínez <taborlindp@gmail.com>
2022-05-17 16:42:36 +0200

    ES6: Default function parameters

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -51,7 +51,7 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
             return res.send({
                 error
@@ -71,26 +71,6 @@ app.get('/weather', (req, res) => {
             })
         })
     })
-    // const url = 'http://api.weatherstack.com/current?access_key=4fa109becd811329cf52f7bd471d3a7c&query=38.5412,0.1234'
-    // request({ url, json: true }, (error, { body }) => {
-    //     if (error) {
-    //         res.send('Unable to connect to weather services.')
-    //     } else if (body.error) {
-    //         res.send('Unable to find location')
-    //     } else {
-    //         res.send({
-    //             location: {
-    //                 city: body.location.name,
-    //                 country: body.location.country
-    //             },
-    //             forecast: {
-    //                 description: body.current.weather_descriptions[0],
-    //                 temperature: body.current.temperature,
-    //                 feelslike: body.current.feelslike
-    //             }
-    //         })
-    //     }
-    // })
 })
 
 app.get('/products', (req, res) => {
