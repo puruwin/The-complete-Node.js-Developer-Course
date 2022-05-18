e79b31db44888b226faca2eb1a0a6184a6c15ad8
David Pérez Martínez <taborlindp@gmail.com>
2022-05-18 13:57:41 +0200

    forecast text changed

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             console.log(body.error)
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like ' + body.current.feelslike + '.')
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature}. It feels like ${body.current.feelslike} and there is a ${body.current.precip}% chance of rain`)
         }
     })
 }
