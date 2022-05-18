7955af6c2eecdda51f0ccf658c7e3cdc32f7bb3c
David Pérez Martínez <taborlindp@gmail.com>
2022-05-18 13:25:12 +0200

    Setup app for Heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.innerHTML = 'Loading...'
     messageTwo.innerHTML = ''
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.innerHTML = data.error
